51c6edf3d16c60800d2ce8fe6cc4830685735a16
cj34920 <cj34920@ly.com>
2018-05-18 18:14:38 +0800

    加入eureka

--- src/main/java/com/example/springbootdemo/controller/TestController.java
+++ src/main/java/com/example/springbootdemo/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.example.springbootdemo.controller;
 
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @author cj34920
@@ -9,8 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+
     @RequestMapping("/test")
-    public  String hello(){
-        return  "Hello World";
+    public String hello() {
+        ServiceInstance serviceInstance = client.getLocalServiceInstance();
+        logger.info("hello,host" + serviceInstance.getHost() + ", server_id" + serviceInstance.getServiceId());
+        return "Hello World";
     }
 }
